d2d3b8bea3c14ff484d0a87c91f480391af6a009
Anger000 <77683727+khue24112000@users.noreply.github.com>
2021-03-24 07:49:42 +0700

    finished

---
+++ QuanLyNhaHang/GUI/UserControls/UC_BanHangNew.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyKhachHang.DTO;
+using QuanLyKhachHang.DAO;
+
+namespace QuanLyKhachHang.GUI.UserControls
+{
+    public partial class UC_BanHangNew : UserControl
+    {
+        public UC_BanHangNew()
+        {
+            InitializeComponent();
+            loadTable();
+        }
+
+        private void btnBan_Click(object sender, EventArgs e)
+        {
+            pageThanhToan.SetPage(tabBan);
+            
+        }
+
+        private void btnThucDon_Click(object sender, EventArgs e)
+        {
+            pageThanhToan.SetPage(tabThucDon);
+        }
+
+        private void btnThanhToan_Click(object sender, EventArgs e)
+        {
+            using(frThanhToan frtt = new frThanhToan())
+            {
+                frtt.ShowDialog();
+            }
+        }
+
+        #region Methods
+        void loadTable()
+        {
+            List<Table> tableList = TableDAO.Instance.loadTableList();
+            foreach (Table item in tableList)
+            {
+                Button btn = new Button() { Width = TableDAO.tableWeight, Height = TableDAO.tableHeight };
+                
+                Font f = new Font("Open Sans", 10, FontStyle.Bold);
+                btn.Font = f;
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.ForeColor = Color.White;
+                btn.Click += btn_Click;
+                btn.Tag = item;
+
+                switch (item.Mapyc)
+                {
+                    case "":
+                        btn.BackColor = Color.FromArgb(233, 137, 126);
+                        btn.Text = "    Bàn " + item.Maban + Environment.NewLine + Environment.NewLine + "Trống";
+                        break;
+                    default:
+                        btn.BackColor = Color.FromArgb(210, 56, 108);
+                        btn.Text = "    Bàn " + item.Maban + Environment.NewLine + Environment.NewLine + "Có người";
+                        break;
+                }
+                fpnlBanAn.Controls.Add(btn);
+            }
+        }
+
+        void showBill(string maban)
+        {
+            List<ChiTietDatMon> ctList = ChiTietDatMonDAO.Instance.getChiTietDatMon(PhieuYeuCauDAO.Instance.getPycByMaban(maban));
+        }
+
+
+        #endregion
+
+        #region Events
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string maban = ((sender as Button).Tag as Table).Maban;
+            showBill(maban);
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ QuanLyNhaHang/GUI/UserControls/UC_DanhMucNew.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachHang.GUI.UserControls.DanhMuc
+{
+    public partial class UC_DanhMucNew : UserControl
+    {
+        public UC_DanhMucNew()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMon_Click(object sender, EventArgs e)
+        {
+            pageDanhMuc.SelectTab(0);
+        }
+
+        private void btnLoai_Click(object sender, EventArgs e)
+        {
+            pageDanhMuc.SelectTab(1);
+        }
+
+        private void btnNhanVien_Click(object sender, EventArgs e)
+        {
+            pageDanhMuc.SelectTab(2);
+        }
+
+        private void btnBan_Click(object sender, EventArgs e)
+        {
+            pageDanhMuc.SelectTab(3);
+        }
+
+        private void btnNguyenLieu_Click(object sender, EventArgs e)
+        {
+            pageDanhMuc.SelectTab(4);
+        }
+
+        private void btnThemNV_Click(object sender, EventArgs e)
+        {
+            using (Add_NhanVien f = new Add_NhanVien())
+                f.ShowDialog();
+        }
+
+        #region Món ăn
+
+        #endregion
+
+
+        #region Loại món
+
+        #endregion
+
+
+        #region Nhân viên
+
+        #endregion
+
+
+        #region Bàn ăn
+
+        #endregion
+
+
+        #region Nguyên liệu
+
+        #endregion
+    }
+}
